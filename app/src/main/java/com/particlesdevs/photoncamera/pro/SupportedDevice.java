89d36db4b1667747f3c5d8995b9ad260bfcd796d
John Galt <johngaltfirstrun@gmail.com>
2021-10-05 11:03:43 -0400

    pro: update urls to dev branch

--- app/src/main/java/com/particlesdevs/photoncamera/pro/SupportedDevice.java
+++ app/src/main/java/com/particlesdevs/photoncamera/pro/SupportedDevice.java
@@ -85,7 +85,7 @@ public class SupportedDevice {
     }
 
     private void loadSupportedDevicesList() throws IOException {
-        BufferedReader in = HttpLoader.readURL("https://raw.githubusercontent.com/eszdman/PhotonCamera/dev/app/SupportedList.txt");
+        BufferedReader in = HttpLoader.readURL("https://raw.githubusercontent.com/RealJohnGalt/PhotonCamera/rebasev2/app/SupportedList.txt");
         String str;
         while ((str = in.readLine()) != null) {
             mSupportedDevicesSet.add(str);
