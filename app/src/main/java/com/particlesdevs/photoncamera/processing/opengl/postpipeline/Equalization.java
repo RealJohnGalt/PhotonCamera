e963ac2b1060f680d3329b47e3a36cc0fd68c98e
John Galt <johngaltfirstrun@gmail.com>
2021-10-11 08:23:04 -0400

    config: bump black level sensitivity

--- app/src/main/java/com/particlesdevs/photoncamera/processing/opengl/postpipeline/Equalization.java
+++ app/src/main/java/com/particlesdevs/photoncamera/processing/opengl/postpipeline/Equalization.java
@@ -496,7 +496,7 @@ public class Equalization extends Node {
     float edgesBilateralSmoothNight = 3.0f;
     float highLightSmoothAmplify = 2.5f;
     float shadowsSensitivity = 0.5f;
-    float blackLevelSensitivity = 1.0f;
+    float blackLevelSensitivity = 1.3f;
     int whiteBalanceSearch = 400;
     float whiteBalanceSaturation = 1.35f;
     float[] tonemapCoeffs = new float[]{-0.78360f / 1.0063f, 0.84690f / 1.0063f, 0.9430f / 1.0063f, 0f};
